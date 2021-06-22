c5748e30cbaf5ed6b8859e88983c760805ed8269
1222ck <rnjsdl5050@naver.com>
2021-06-22 16:42:59 +0900

    update

--- networksession/server_http.js
+++ networksession/server_http.js
@@ -19,3 +19,21 @@ app.post("/", (req, res) => {
   res.send(`post value is : ` + req.body.Client + ``);
 });
 console.log("server on");
+
+var io = require("socket.io")(server);
+var roomName;
+io.on("connection", function (socket) {
+  console.log("connect");
+  var instanceId = socket.id;
+  socket.on("joinRoom", function (data) {
+    console.log(data);
+    socket.join(data.roomName);
+    roomName = data.roomName;
+  });
+  socket.on("reqMsg", function (data) {
+    console.log(data);
+    io.sockets
+      .in(roomName)
+      .emit("recMsg", { comment: Id + " : " + data.comment + "\n" });
+  });
+});
